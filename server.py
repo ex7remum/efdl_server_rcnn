eafc351014e3ff32066299cf1aa92bb3e8dcd524
Yuriy Maksyuta <yura-maksuta@mail.ru>
2024-03-18 04:16:51 +0300

    server fix

--- server.py
+++ server.py
@@ -15,22 +15,22 @@ import io
 app = Flask(__name__, static_url_path="")
 metrics = PrometheusMetrics(app)
 
-model = torchvision.models.detection.maskrcnn_resnet50_fpn()
-model.load_state_dict(torch.load('maskrcnn_resnet50_fpn.pt'))
-model.eval()
-with open('labels.json', 'r') as f:
-    labels_raw = json.loads(f.read())
-    labels = {int(index): value for index, value in enumerate(labels_raw)}
-
 
 @app.route("/predict", methods=['POST'])
 @metrics.counter("app_http_inference_count", "number of invocations")
 def predict():
+    model = torchvision.models.detection.maskrcnn_resnet50_fpn()
+    model.load_state_dict(torch.load('maskrcnn_resnet50_fpn.pt'))
+    model.eval()
+    with open('labels.json', 'r') as f:
+        labels_raw = json.loads(f.read())
+        labels = {int(index): value for index, value in enumerate(labels_raw)}
 
     url = request.get_json(force=True)["url"]
     response = requests.get(url)
     data = Image.open(io.BytesIO(response.content))
-    tensor_data = transforms.ToTensor(data).unsqueeze(0)
+    transform = transforms.ToTensor()
+    tensor_data = transform(data).unsqueeze(0)
     batch = convert_image_dtype(tensor_data, dtype=torch.float)
 
     with torch.no_grad():
@@ -41,7 +41,7 @@ def predict():
     res_labels = []
     for label, score in zip(cur_labels, scores):
         if score > 0.75:
-            res_labels.append(labels[label])
+            res_labels.append(labels[label.item()])
 
     return jsonify({
         "objects": res_labels
